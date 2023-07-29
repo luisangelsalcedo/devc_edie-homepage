b0cbca2827d9b58a5db3f82f39f2f856c267a673
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-07-29 17:31:36 -0500

    🍔 Add CollapseMenu component

---
+++ src/components/collapsed-menu/icon.js
@@ -0,0 +1,4 @@
+import IconOpen from './IconOpen.astro';
+import IconClose from './IconClose.astro';
+
+export { IconOpen, IconClose };
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import MainMenu from './main-menu/MainMenu.astro';
 import Brand from './brand/Brand.astro';
 import ContactUs from './contact-us/ContactUs.astro';
 import SocialMedia from './social-media/SocialMedia.astro';
+import CollapsedMenu from './collapsed-menu/CollapsedMenu.astro';
 
-export { MainMenu, Brand, ContactUs, SocialMedia };
+export { MainMenu, Brand, ContactUs, SocialMedia, CollapsedMenu };
