338f2348f2ca4c484a49ecc354df43a711f3435b
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-07-29 14:04:17 -0500

    🦴 Add first HTML structure

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import MainMenu from './main-menu/MainMenu.astro';
+import Brand from './brand/Brand.astro';
+import ContactUs from './contact-us/ContactUs.astro';
+import SocialMedia from './social-media/SocialMedia.astro';
+
+export { MainMenu, Brand, ContactUs, SocialMedia };
