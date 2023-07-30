8284855ae4752aa7d579fcbd72afc87e6ab6ef9f
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-07-30 02:20:00 -0500

    💼 Add our work section

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,5 +1,6 @@
 import IconEdit from './IconEdit.astro';
 import IconCode from './IconCode.astro';
 import IconServer from './IconServer.astro';
+import Arrow from './Arrow.astro';
 
-export { IconEdit, IconCode, IconServer };
+export { IconEdit, IconCode, IconServer, Arrow };
