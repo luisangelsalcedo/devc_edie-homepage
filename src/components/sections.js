ecc8ee91018b195564a83a76652fc380e34a5a85
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-07-29 19:19:42 -0500

    🔪 Dividing the sections into components

---
+++ src/components/sections.js
@@ -0,0 +1,13 @@
+import SectionAboutUs from './section-about-us/SectionAboutUs.astro';
+import SectionService from './section-service/SectionService.astro';
+import SectionOurWork from './section-our-work/SectionOurWork.astro';
+import SectionTeam from './section-team/SectionTeam.astro';
+import SectionClient from './section-client/SectionClient.astro';
+
+export {
+  SectionAboutUs,
+  SectionService,
+  SectionOurWork,
+  SectionTeam,
+  SectionClient,
+};
